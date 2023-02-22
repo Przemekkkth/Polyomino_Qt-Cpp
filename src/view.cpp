03e439dc9607859c3cdb3c0a1065cb77558f9215
Bondrusiek <przemekkth@outlook.com>
2023-02-22 14:23:06 +0100

    add commented making screenshots.

--- src/view.cpp
+++ src/view.cpp
@@ -1,6 +1,8 @@
 #include "view.h"
 #include <QKeyEvent>
 #include <QApplication>
+#include <QDir>
+#include <QPainter>
 
 View::View()
     : m_gameScene(new GameScene(this)),
@@ -32,6 +34,19 @@ void View::keyReleaseEvent(QKeyEvent *event)
     if(event->key() == Qt::Key_Escape)
     {
         QApplication::instance()->quit();
+    }
+    if(event->key() == Qt::Key_Z)
+    {
+        //Uncomment below if you want to make screenshots
+//        static int index = 0;
+//        QString fileName = QDir::currentPath() + QDir::separator() + "screen" + QString::number(index++) + ".png";
+//        QRect rect = sceneRect().toAlignedRect();
+//        QImage image(rect.size(), QImage::Format_ARGB32);
+//        image.fill(Qt::transparent);
+//        QPainter painter(&image);
+//        render(&painter);
+//        image.save(fileName);
+//        qDebug() << "saved " << fileName;
     }
     QGraphicsView::keyReleaseEvent(event);
 }
